04ece84ce2e142cc280e5d1b4770b8f29d3d7cc3
juliataitz <juliataitz@gmail.com>
2015-06-29 17:02:36 -0400

    Updated spec and changed methods to take parameter

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -1,70 +1,53 @@
 #code your solutions here
-require 'pry'
 
 #1
-def recipe_ingredients
+
   mac_and_cheese_recipe = {
     :elbow_pasta => "2 cups",
     :butter => "1/2 stick",
     :whole_milk => "1 cup",
     :shredded_cheddar_cheese => "3 cups"
   }
+
+ #2
+
+def recipe_ingredients(mac_and_cheese_recipe)
+	mac_and_cheese_recipe
+
 end
 
 #3:
-def amount_of_cheese
+def amount_of_cheese(mac_and_cheese_recipe)
   # you'll need to include your whole hash in this method on the lines before the puts
   #code your solution here directly after the puts
-  mac_and_cheese_recipe = {
-    :elbow_pasta => "2 cups",
-    :butter => "1/2 stick",
-    :whole_milk => "1 cup",
-    :shredded_cheddar_cheese => "3 cups"
-  }
 
   mac_and_cheese_recipe[:shredded_cheddar_cheese]
 
 end
 
 #4:
-def ingredients_and_amounts
+def ingredients_and_amounts(mac_and_cheese_recipe)
   #you'll need to include your whole hash in this method
   #code your solution here
-  mac_and_cheese_recipe = {
-    :elbow_pasta => "2 cups",
-    :butter => "1/2 stick",
-    :whole_milk => "1 cup",
-    :shredded_cheddar_cheese => "3 cups"
-  }
   mac_and_cheese_recipe.each do |key, value|
     puts "#{key} #{value}"
   end
+
 end
 
 
 #5:
-def ingredients
+def ingredients(mac_and_cheese_recipe)
   #you'll need to include your whole hash in this method
   #code your solution here
-  mac_and_cheese_recipe = {
-    :elbow_pasta => "2 cups",
-    :butter => "1/2 stick",
-    :whole_milk => "1 cup",
-    :shredded_cheddar_cheese => "3 cups"
-  }
+
   mac_and_cheese_recipe.each_key { |key| puts key }
 end
 
 #6:
-def amounts
+def amounts(mac_and_cheese_recipe)
   #you'll need to include your whole hash in this method
   #code your solution here
-  mac_and_cheese_recipe = {
-    :elbow_pasta => "2 cups",
-    :butter => "1/2 stick",
-    :whole_milk => "1 cup",
-    :shredded_cheddar_cheese => "3 cups"
-  }
 
   mac_and_cheese_recipe.each_value do |value|
     puts value
@@ -72,15 +55,9 @@ def amounts
 end
 
 #7:
-def number_ingredients
+def number_ingredients(mac_and_cheese_recipe)
   #you'll need to include your whole hash in this method
   #code your solution here
-  mac_and_cheese_recipe = {
-    :elbow_pasta => "2 cups",
-    :butter => "1/2 stick",
-    :whole_milk => "1 cup",
-    :shredded_cheddar_cheese => "3 cups"
-  }
 
   mac_and_cheese_recipe.length
 end
