124ee036b50dec8b6ac6708862e00c637e1a2f9c
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2017-06-21 23:43:18 +0000

    here it isgit add .

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -13,35 +13,55 @@
 #  - agave - 1/4 cup
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 
+perfect_10_recipe = {
+    :almond_flour => "2 cups",
+    :gluten_free_whole_oats => "1 cup",
+    :kosher_salt => "1/2 tea spoon",
+    :baking_powder => "1/2 tea spoon",
+    :mini_dark_chocolate_chips => "3/4 cup"
+}
+
 
 
 
 #2. Write a method that simply returns (not puts) your perfect_10 hash
 def recipe_ingredients(recipe_hash)
-
+    return recipe_hash
 end
 
-#2: Write code that returns the amount of chocolate chips from perfect_10 ingredients that is needed in the recipe.
+
+#2: Write code that returns    the amount of chocolate chips    from perfect_10 ingredients that is needed in the recipe.
 def amount_of_chocolate_chips(recipe_hash)
-
+    recipe_hash[:mini_dark_chocolate_chips]
 end
+
+# puts "What ingredient do you want?"
+# ing = gets.chomp
+# puts "You need #{perfect_10_recipe[ing.to_sym]} of #{ing}."
 
 #3: Use `puts` to print out every ingredient and amount in the hash by iterating through the collection. You'll want to use the `each` method.
 def ingredients_and_amounts(recipe_hash)
-
+    recipe_hash.each do |ingredient, amount|
+        puts "#{ingredient} #{amount}"
+    end
 end
 
 #4: Now use `puts` to print out just the ingredients without the amounts. You'll want to use the `each_key` method.
 def ingredients(recipe_hash)
-
+    recipe_hash.each_key do |ingredient| 
+        puts ingredient
+    end
 end
 
 #5: Now use `puts` to print out just the amounts without the ingredients. You'll want to use the `each_value` method.
 def amounts(recipe_hash)
+    recipe_hash.each_value do |amount|
+       puts amount 
+    end
   
 end
 
 #6: Return the number of ingredients in the `perfect_10_recipe` hash.
 def number_ingredients(perfect_10_recipe)
-
+    perfect_10_recipe.length
 end
